9532100906a3901af4d2d5297f2a0ac7497d538c
nemanja-m <nemanja_milicevic@hotmail.com>
2016-09-15 21:55:42 +0200

    Add iterative DFS

---
+++ algorithms/depth-first-search/dfs.hpp
@@ -0,0 +1,34 @@
+#ifndef __DFS__
+#define __DFS__
+
+#include "stack"
+#include "graph.hpp"
+#include "stdexcept"
+#include "iostream"
+
+void dfs(Graph *graph, int start) {
+  if (!graph->exists(start))
+    throw std::runtime_error("Key doesn't exists!");
+
+  // Mark all vertices as unvisited
+  bool visited[graph->vertices.size()];
+  for (bool &v : visited)
+    v = false;
+
+  // Push initial vertex
+  std::stack<Vertex *> front;
+  front.push(graph->vertices[start]);
+
+  while (!front.empty()) {
+    Vertex *vertex = front.top();
+    visited[vertex->key] = true;
+    std::cout << vertex->key << std::endl;
+    front.pop();
+
+    for (auto iter = vertex->edges.begin(); iter != vertex->edges.end(); iter++)
+      if (!visited[*iter])
+        front.push(graph->vertices[*iter]);
+  }
+}
+
+#endif
--- algorithms/depth-first-search/graph.hpp
+++ algorithms/depth-first-search/graph.hpp
@@ -8,17 +8,14 @@ struct Vertex {
   int key;
   std::vector<int> edges;
 
-  Vertex(int key) {
-    this->key = key;
-  }
-
+  Vertex(int key) : key(key) { }
 };
 
 struct Graph {
   std::map<int, Vertex *> vertices;
 
   void add_vertex(const int key) {
-    if (vertices.find(key) == vertices.end()) {
+    if (!exists(key)) {
       vertices[key] = new Vertex(key);
     }
   }
@@ -26,6 +23,15 @@ struct Graph {
   void add_edge(const int from, const int to) {
     Vertex *f = vertices.find(from)->second;
     f->edges.push_back(to);
+  }
+
+  bool exists(int key) {
+    return vertices.find(key) != vertices.end();
+  }
+
+  ~Graph() {
+    for (auto &kv : vertices)
+      delete kv.second;
   }
 };
 
