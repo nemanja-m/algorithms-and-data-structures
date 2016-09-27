bb0799ce600aaab5bce2ea817a8f98aef5c5fcaa
nemanja-m <nemanja_milicevic@hotmail.com>
2016-09-27 21:04:02 +0200

    Add Makefile

--- data-structures/stack/test.cpp
+++ data-structures/stack/test.cpp
@@ -7,18 +7,21 @@ int main(int argc, char** argv) {
   Stack<int> stack;
 
   cout << "\n-- Push --" << endl;
+  cout << "> 43 pushed" << endl;
+  cout << "> 23 pushed" << endl;
   stack.push(43);
   stack.push(23);
 
-  cout << "\n\n-- Top/Pop --" << endl;
+  cout << "\n-- Top/Pop --" << endl;
 
   cout << stack.top() << endl; stack.pop();
   cout << stack.top() << endl; stack.pop();
 
-  cout << "\n\n-- Clear --" << endl;
+  cout << "\n-- Clear --" << endl;
   stack.clear();
+  cout << "> stack cleared" << endl;
 
-  cout << "\n\n-- Size --" << endl;
+  cout << "\n-- Size --" << endl;
   cout << "Size: " << stack.size() << endl;
 
   return 0;
