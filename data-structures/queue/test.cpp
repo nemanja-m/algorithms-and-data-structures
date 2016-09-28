72a5be9c0fed2694ce26a2aca1ff7e3c5a413ee5
nemanja-m <nemanja_milicevic@hotmail.com>
2016-09-28 23:35:09 +0200

    Clear queue

--- data-structures/queue/test.cpp
+++ data-structures/queue/test.cpp
@@ -8,18 +8,26 @@ int main(int argc, char** argv) {
 
   cout << "\n-- Enqueuing --" << endl;
 
-  for (int i = 0; i < 16; i++) {
+  for (int i = 0; i < 17; i++) {
     cout << "--> " << i << endl;
     queue.enqueue(i);
   }
 
-  cout << "\nCapacity: " << queue.capacity() << endl;
+  cout << "\nSize: " << queue.size() << endl;
+  cout << "Capacity: " << queue.capacity() << endl;
 
   cout << "\n-- Dequeuing --" << endl;
   while (!queue.empty())
     cout << queue.dequeue() << " ";
 
-  cout << endl;
+  cout << "\n\n--> 43\n--> 23" << endl;
+  queue.enqueue(43);
+  queue.enqueue(23);
+
+  cout << "\n-- Clear queue --" << endl;
+  queue.clear();
+
+  cout << "Size: " << queue.size() << endl;
 
   return 0;
 }
