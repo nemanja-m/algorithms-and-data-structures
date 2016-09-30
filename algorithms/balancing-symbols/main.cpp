204ed3c23d069acc1d341930365f0f4aa6cd9ee2
nemanja-m <nemanja_milicevic@hotmail.com>
2016-09-30 23:11:45 +0200

    Mark unbalanced symbol

--- algorithms/balancing-symbols/main.cpp
+++ algorithms/balancing-symbols/main.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+string mark_unbalanced(const string &symbols, size_t idx) {
+  string ret;
+
+  for (size_t i = 0; i < symbols.size(); ++i)
+    if (i == idx)
+      ret.append("\e[31m" + string(1, symbols[i]) + "\e[0m");
+    else
+      ret.push_back(symbols[i]);
+
+  return ret;
+}
+
 int main(int argc, char **argv) {
   if (argc == 2) {
     string symbols(argv[1]);
@@ -10,9 +22,9 @@ int main(int argc, char **argv) {
     Result result = balance(symbols);
 
     if (result.balanced)
-      cout << "Balanced" << endl;
+      cout << "\n-- \e[32mBalanced\e[0m --\n\n";
     else
-      cout << "Unbalanced: " << result.index << endl;
+      cout << "\n-- \e[31mUnbalanced:\e[0m " << mark_unbalanced(symbols, result.index)  << " --\n\n";
 
   } else {
     cerr << "Invalid number of arguments: i.e. ./balance \"{()}\"" << endl;
